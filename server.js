7977181a8f77f479bf07d6591565a7e068794ff7
ElenaValladolid <ehmenoyo@gmail.com>
2024-06-21 16:06:18 +0200

    devcam_repaso

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//carga el archivo de config.env en dotenv
+dotenv.config({path: './config/config.env'});
+
+//crear objeto app a partir de la clase express()
+const app = express();
+
+//determinar el puerto de ejecucion del server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server en modo ${process.env.NODE_ENV}en el puerto ${PORT}`));
+
+
+
